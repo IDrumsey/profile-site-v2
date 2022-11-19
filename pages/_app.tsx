199d87eca6c40383fe2ff3429affae6c9420bee9
IDrumsey <clayofcasper@gmail.com>
2022-11-18 20:28:17 -0500

    Added custom dark theme.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colors: {
+          dark: ['#D4D4D4', '#848484', '#505050', '#343434', '#1E1E1E', '#1A1A1A', '#161616', '#131313', '#101010']
+        },
+        colorScheme: 'dark'
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
