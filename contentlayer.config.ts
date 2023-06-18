322dba1fd2339a7dd0ea1536aa7bedb37699154e
IDrumsey <claydevelop@gmail.com>
2023-06-18 16:23:37 -0400

    Added remark-gfm for github md extensions.

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,5 +1,6 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
 import remarkToc from "remark-toc"
+import remarkGfm from "remark-gfm"
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -27,6 +28,6 @@ export default makeSource({
   contentDirPath: "posts",
   documentTypes: [Post],
   mdx: {
-    remarkPlugins: [remarkToc],
+    remarkPlugins: [remarkToc, remarkGfm],
   },
 })
