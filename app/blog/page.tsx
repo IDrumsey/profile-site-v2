f2f77d196bc29b4a64fd77208871391ec70c69f4
IDrumsey <claydevelop@gmail.com>
2023-06-23 23:39:23 -0400

    Revert "Show newer posts first."
    This reverts commit 5fc4a162a7255ede7ca490e1811f59cec60517dd.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/_blog.module.scss"
 import Link from "next/link"
-import { compareAsc } from "date-fns"
+import { compareDesc, format, parseISO } from "date-fns"
 import { allPosts, Post } from "contentlayer/generated"
 import BlogPostCard from "@/components/blog-post-card/blog-post-card"
 
@@ -8,7 +8,7 @@ type Props = {}
 
 const BlogPage = ({}: Props) => {
   const posts = allPosts.sort((a, b) =>
-    compareAsc(new Date(a.date), new Date(b.date))
+    compareDesc(new Date(a.date), new Date(b.date))
   )
 
   return (
