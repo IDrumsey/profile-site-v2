2af15b5240877ec011fcbde79ff351d2b2c9835f
IDrumsey <claydevelop@gmail.com>
2023-06-23 21:52:35 -0400

    Added pretty code

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,5 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
-import remarkToc from "remark-toc"
-import remarkGfm from "remark-gfm"
+import rehypePrettyCode from "rehype-pretty-code"
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -28,6 +27,29 @@ export default makeSource({
   contentDirPath: "posts",
   documentTypes: [Post],
   mdx: {
-    remarkPlugins: [remarkToc, remarkGfm],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          keepBackground: true,
+          onVisitLine(element: any) {
+            // Prevent lines from collapsing in `display: grid` mode, and
+            // allow empty lines to be copy/pasted
+            if (element.children.length === 0) {
+              element.children = [{ type: "text", value: " " }]
+            }
+          },
+          onVisitHighlightedLine(element: any) {
+            // Each line element by default has `class="line"`.
+            element.properties.className?.push("highlighted")
+          },
+          onVisitHighlightedWord(element: any) {
+            // Each word element has no className by default.
+            element.properties.className = ["word"]
+          },
+        },
+      ],
+    ],
   },
 })
