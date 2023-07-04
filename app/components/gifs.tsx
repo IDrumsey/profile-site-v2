893da2d54fe0086f9abe9e9424849e6e13040462
IDrumsey <claydevelop@gmail.com>
2023-07-04 16:41:04 -0400

    Wrote check dependencies blog.

--- app/components/gifs.tsx
+++ app/components/gifs.tsx
@@ -2,11 +2,32 @@ import GiphyGif from "./giphy-gif/giphy-gif"
 
 export const HeadShakeNoGif = () => {
   return (
-    <GiphyGif options={{ style: { margin: "auto", marginTop: 25 } }}>
+    <GiphyGif
+      options={{ style: { margin: "auto", marginTop: 25 } }}
+      link="https://giphy.com/gifs/cEGwQzpKFKcUw"
+    >
       <iframe
         src="https://giphy.com/embed/cEGwQzpKFKcUw"
         width="480"
         height="214"
+        frameBorder="0"
+        className="giphy-embed"
+        allowFullScreen
+      ></iframe>
+    </GiphyGif>
+  )
+}
+
+export const FallOnFaceGif = () => {
+  return (
+    <GiphyGif
+      options={{ style: { margin: "auto", marginTop: 25 } }}
+      link="https://giphy.com/gifs/falling-bike-face-plant-hWgeMEUncId9u"
+    >
+      <iframe
+        src="https://giphy.com/embed/hWgeMEUncId9u"
+        width="480"
+        height="308"
         frameBorder="0"
         className="giphy-embed"
         allowFullScreen
