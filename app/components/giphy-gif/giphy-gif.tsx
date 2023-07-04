a20618a4698e93475cff520385271c3d9747bdc4
IDrumsey <claydevelop@gmail.com>
2023-07-04 16:40:45 -0400

    Updated gif component to take source.

--- app/components/giphy-gif/giphy-gif.tsx
+++ app/components/giphy-gif/giphy-gif.tsx
@@ -3,14 +3,19 @@ import { PropsWithChildren, HTMLProps } from "react"
 
 export type GiphyGifProps = {
   options?: HTMLProps<HTMLDivElement>
+  link: string
 }
 
-const GiphyGif = ({ children, options }: PropsWithChildren<GiphyGifProps>) => {
+const GiphyGif = ({
+  children,
+  options,
+  link,
+}: PropsWithChildren<GiphyGifProps>) => {
   return (
     <div style={{ width: "max-content", ...options?.style }}>
       {children}
       <p style={{ fontSize: 15, color: "#7d7d7d", marginTop: -25 }}>
-        <a href="https://giphy.com/gifs/cEGwQzpKFKcUw">via GIPHY</a>
+        <a href={link}>via GIPHY</a>
       </p>
     </div>
   )
